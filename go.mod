4182c6ef39a3ce65fa12f0b9d199759864ed537f
Eugen Batrak <com.rev@mail.ru>
2022-03-28 20:28:32 +0300

    net in wi-fi example

--- go.mod
+++ go.mod
@@ -1,12 +1,3 @@
 module qartbot
 
 go 1.17
-
-require github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e
-
-require (
-	github.com/Syfaro/telegram-bot-api v4.6.4+incompatible // indirect
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
-	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/tour v0.1.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,36 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"image/color"
 	"log"
 	"net"
-	"os"
-	"runtime"
-	"sync"
 	"time"
-
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/skip2/go-qrcode"
 )
-
-type TelegramToken struct {
-	BotToken string
-}
-
-func Pic(dx, dy int) [][]uint8 {
-	img := make([][]uint8, dy)
-	for y := range img {
-		img[y] = make([]uint8, dx)
-		for x := range img[y] {
-			img[y][x] = uint8(x*3 + y*2)
-		}
-	}
-	return img
-}
-
-var wg sync.WaitGroup
 
 func main() {
 	// local message
@@ -42,7 +17,6 @@ func main() {
 	fmt.Println(listener)
 
 	// ожидаю новые подключения "слушать порт"
-
 	connection, err := listener.Accept()
 	if err != nil {
 		log.Fatal(err)
@@ -63,37 +37,4 @@ func main() {
 	fmt.Println(string(msg_in))
 
 	//у клиента не завершилось соедение без отправки пакета на завершение
-	file, err := os.Open("token.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(runtime.NumCPU())
-	decoder := json.NewDecoder(file)
-	token := TelegramToken{}
-	decoder.Decode(&token)
-
-	wg.Add(2)
-	go math(10) //рутина
-	go math(20)
-	fmt.Println(token.BotToken)
-
-	bot, err := tgbotapi.NewBotAPI(token.BotToken)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-	wg.Wait()
-	qrcode.WriteColorFile("https://www.example.com", qrcode.Medium, 256, color.CMYK{200, 20, 60, 10}, color.White, "qr.png")
-}
-
-func math(id int) {
-	defer wg.Done()
-	for i := 1; i < 4; i++ {
-
-		fmt.Printf("%d:%d\n", id, i)
-		time.Sleep(time.Second)
-
-	}
 }
