86de0ed9e2d8a9243f3f081154fb32a17e2da4c4
Eugen Bardak <com.rev@mail.ru>
2022-03-29 13:40:48 +0300

    added few functions

--- main.go
+++ main.go
@@ -7,34 +7,44 @@ import (
 	"time"
 )
 
+func connection(client net.Listener) {
+	// ожидаю новые подключения "слушать порт"
+	connection, err := client.Accept()
+	if err != nil {
+		log.Fatal(err)
+	}
+	timeout := time.Now().Add(time.Second)
+	connection.SetReadDeadline(timeout)
+
+	fmt.Println("\nConnection #", connection)
+
+	// defer - выполнить по завершении программы
+	// направить пакет клиенту о заверешении соединения
+	defer connection.Close()
+}
+
+func sendMsg(client net.Conn, msg string) {
+	msg_out := []byte("Prvet Misha")
+	client.Write(msg_out)
+}
+
+func getMsg(client net.Conn) string {
+	msg := make([]byte, 80)
+	_, err := client.Read(msg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(msg))
+	return string(msg)
+}
+
 func main() {
-	// local message
 	//lister - серверный сокет
 	listener, err := net.Listen("tcp4", "192.168.1.62:1027")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(listener)
-
-	// ожидаю новые подключения "слушать порт"
-	connection, err := listener.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(connection)
-	defer connection.Close()
-	t := time.Now().Add(time.Second)
-	connection.SetReadDeadline(t)
-	msg_out := []byte("Prvet Misha")
-	msg_in := make([]byte, 10)
-
-	connection.Write(msg_out)
-	n, err := connection.Read(msg_in)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(n)
-	fmt.Println(string(msg_in))
-
-	//у клиента не завершилось соедение без отправки пакета на завершение
 }
