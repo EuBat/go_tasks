d8c3a120ff663eef9be595fbbb0cf957bd35a98c
Eugen Bardak <com.rev@mail.ru>
2022-03-30 15:57:29 +0300

    add methods

--- main.go
+++ main.go
@@ -4,33 +4,30 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
+	"sync"
 )
 
-func connection(client net.Listener) {
-	// ожидаю новые подключения "слушать порт"
-	connection, err := client.Accept()
-	if err != nil {
-		log.Fatal(err)
-	}
-	timeout := time.Now().Add(time.Second)
-	connection.SetReadDeadline(timeout)
+// как сделать переменные сеттеров cosnt
+// как сделать геттеры const функцией
+// где деструктор :(
+// конструктора нет. деструктура тоже нет.
+var wg sync.WaitGroup
 
-	fmt.Println("\nConnection #", connection)
-
-	// defer - выполнить по завершении программы
-	// направить пакет клиенту о заверешении соединения
-	defer connection.Close()
+type Client struct {
+	Connection net.Conn
+	status     bool
 }
 
-func sendMsg(client net.Conn, msg string) {
-	msg_out := []byte("Prvet Misha")
-	client.Write(msg_out)
+func (c *Client) SendMsg(message string) {
+	msg := []byte(message)
+	c.Connection.Write(msg)
 }
 
-func getMsg(client net.Conn) string {
+func (c *Client) GetMsg() string {
+
 	msg := make([]byte, 80)
-	_, err := client.Read(msg)
+
+	_, err := c.Connection.Read(msg)
 
 	if err != nil {
 		log.Fatal(err)
@@ -40,11 +37,52 @@ func getMsg(client net.Conn) string {
 	return string(msg)
 }
 
-func main() {
-	//lister - серверный сокет
-	listener, err := net.Listen("tcp4", "192.168.1.62:1027")
+func (c *Client) Connect(server net.Listener) {
+	var err error
+	c.Connection, err = server.Accept()
 	if err != nil {
 		log.Fatal(err)
 	}
+	c.SendMsg("Hi Eugen")
+	//timeout := time.Now().Add(time.Second)
+	//c.Connection.SetReadDeadline(timeout)
+	fmt.Println("\nConnection #", c.Connection)
+}
+
+func (c *Client) ConnectClose() {
+	c.Connection.Close()
+	c.SendMsg("bye-bye")
+}
+
+func main() {
+	//lister - серверный сокет
+	listener, err := net.Listen("tcp4", "172.20.10.2:1027")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("error", err)
+	}
 	fmt.Println(listener)
+	fmt.Println("ok")
+
+	clients := make([]Client, 10)
+
+	wg.Add(5)
+	go clients[0].Connect(listener)
+	go clients[1].Connect(listener)
+	go clients[2].Connect(listener)
+	go clients[3].Connect(listener)
+	go clients[4].Connect(listener)
+
+	// for i := range clients {
+	// 	fmt.Println("/n#G:", i)
+	// 	fmt.Println("/n#:", clients[i].Connection)
+	// 	defer wg.Done()
+	// }
+
+	// for i := range clients {
+	// 	go clients[i].SendMsg("bye-bye")
+	// 	go clients[i].ConnectClose()
+	// }
+
+	wg.Wait()
 }
